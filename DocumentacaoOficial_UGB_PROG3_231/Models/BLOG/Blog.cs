06d8d842fd0db1fd69294289d7d6efa1212752a3
danielvasti <danielvasticr@gmail.com>
2023-05-15 19:34:29 -0300

    exercicio p/ dia 16

--- DocumentacaoOficial_UGB_PROG3_231/Models/BLOG/Blog.cs
+++ DocumentacaoOficial_UGB_PROG3_231/Models/BLOG/Blog.cs
@@ -4,6 +4,7 @@ namespace DocumentacaoOficial_UGB_PROG3_231.Models.BLOG
 {
     public class Blog
     {
+        public int Id { get; set; }
         public string Nome { get; set; }
         public virtual Uri SiteUri { get; set; }
         public ICollection<Post> Posts { get; }
--- DocumentacaoOficial_UGB_PROG3_231/Models/POST/Post.cs
+++ DocumentacaoOficial_UGB_PROG3_231/Models/POST/Post.cs
@@ -4,6 +4,7 @@ namespace DocumentacaoOficial_UGB_PROG3_231.Models.POST
 {
     public class Post
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
         public DateTime PublishedOn { get; set; }
